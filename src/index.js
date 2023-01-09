1eeef8d6509a63d45e75a2ec051331daffaeae6d
Artur <kuchinski21@inbox.ru>
2023-01-09 22:20:32 +0300

    init: reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    const mathNumber = Math.abs(n)
+  const strNum = mathNumber + ''
+  const reverseArray = strNum.split('').reverse().join('')
+  return +reverseArray
 }
